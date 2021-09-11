f41ce6a3d200e6b9ec094f0372f87fcb47753bd6
Gustavo Martins <gucmartins@gmail.com>
2021-09-11 13:29:27 -0300

    Include transform controls in added geometries

--- src/app/lib/threeWorld/World.ts
+++ src/app/lib/threeWorld/World.ts
@@ -2,37 +2,35 @@ import { createCamera } from './components/camera';
 import { createGeometry } from './components/geometry';
 import { createScene } from './components/scene';
 
-import { createControls } from './systems/controls';
+import { createOrbitControl, createTransformControl } from './systems/controls';
 import { createRenderer } from './systems/renderer';
 import { Resizer } from './systems/Resizer';
 import { Loop } from './systems/Loop';
 import { AxesHelper, GridHelper, HemisphereLightProbe } from 'three';
 import { Geometry } from 'src/app/model/geometry';
 
-
 const camera = createCamera();
 const renderer = createRenderer();
 const scene = createScene();
 const loop = new Loop(camera, scene, renderer);
-const controls = createControls(camera, renderer.domElement);
-const light = new HemisphereLightProbe(0xffffbb, 0x080820, 1 );
+const orbitControl = createOrbitControl(camera, renderer.domElement);
+const light = new HemisphereLightProbe(0xffffbb, 0x080820, 1);
 
 const axes = new AxesHelper(1);
 //const helper = new DirectionalLightHelper(light, 5);
 const grid = new GridHelper();
 scene.add(light, axes, grid);
 
-
 export class World {
   private sceneElements: any[] = [];
-  
+
   constructor() {}
-  
-  bind (container: HTMLElement) {
+
+  bind(container: HTMLElement) {
     container.append(renderer.domElement);
-    
+
     // loop.updatables.push(controls);
-  
+
     const resizer = new Resizer(container, camera, renderer);
   }
 
@@ -51,16 +49,27 @@ export class World {
 
   addGeometry(geometry: Geometry) {
     const geo = createGeometry(geometry.name, geometry.color);
+    const control = createTransformControl(camera, renderer.domElement, orbitControl);
+    control.attach(geo);
+
     const pos = geometry.position;
-    geo.position.set(pos.x,pos.y,pos.z);
-    scene.add(geo);
-    this.sceneElements.push(geo);
+    geo.position.set(pos.x, pos.y, pos.z);
+    
+    scene.add(geo, control);
+    
+    this.sceneElements.push({
+      geometry: geo,
+      control: control,
+    });
     return geo;
   }
 
   deleteGeometry(id: any) {
-    let index = this.sceneElements.findIndex(elem => elem.id == id);
-    scene.remove(this.sceneElements[index]);
-    this.sceneElements.splice(index,1);
+    let index = this.sceneElements.findIndex((elem) => elem.geometry.id == id);
+    scene.remove(
+      this.sceneElements[index].geometry,
+      this.sceneElements[index].control
+    );
+    this.sceneElements.splice(index, 1);
   }
 }
--- src/app/lib/threeWorld/systems/controls.ts
+++ src/app/lib/threeWorld/systems/controls.ts
@@ -1,7 +1,8 @@
 import { Camera } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls';
 
-export function createControls(camera: Camera, canvas: HTMLElement) {
+export function createOrbitControl(camera: Camera, canvas: HTMLElement) {
   const controls = new OrbitControls(camera, canvas);
 
   // damping and auto rotation require
@@ -13,5 +14,13 @@ export function createControls(camera: Camera, canvas: HTMLElement) {
   // controls.tick = () => controls.update();
 
   return controls;
+}
+
+export function createTransformControl(camera: Camera, canvas: HTMLElement, orbitControl: OrbitControls) {
+  const control = new TransformControls(camera, canvas);
+  control.addEventListener('dragging-changed', function (event) {
+    orbitControl.enabled = !event.value;
+  });
+  return control;
 }
 
